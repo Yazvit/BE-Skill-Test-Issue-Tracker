69a7b03f0540b78ebbaf79fc1de63e4bc1c00f4f
Yazvit <rgyazhinirks@gmail.com>
2024-01-29 09:57:24 +0530

    update

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,10 @@
-
 const mongoose = require("mongoose")
 
 // Making Connecting with mongoDB
 const connectDb = async ()=>{
     try{
-        const connect = await mongoose.connect("mongodb+srv://admin:admin@shubhamcluster.ieolx9t.mongodb.net/IssueTracker?retryWrites=true&w=majority")
+        const connect = await mongoose.connect("mongodb+srv://yazvits:QWE0sH8Y61UtP45G@atlascluster.aijnrnd.mongodb.net/IssueTracker")
+        
         console.log("Database Connected: ", connect.connection.name)
     }
     catch(err){
